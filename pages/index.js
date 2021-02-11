93aa62bee2f51a0ad1d2eaa802d9db4166dbca70
Kennaay <twahl1991@gmail.com>
2021-02-11 01:07:19 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from 'C:/Users/twahl/nextjs-blog/components/layout'
-import utilStyles from 'C:/Users/twahl/nextjs-blog/styles/utils.module.css'
-import { getSortedPostsData } from 'C:/Users/twahl/nextjs-blog/lib/posts'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
-import Date from 'C:/Users/twahl/nextjs-blog/components/date'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
